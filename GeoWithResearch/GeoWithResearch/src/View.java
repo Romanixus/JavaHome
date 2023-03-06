4546e5a0f9a1946cb7b723cd199a23d08326f77d
Роман Александрович Огарков <113164295+Romanixus@users.noreply.github.com>
2023-03-06 09:54:57 +0200

    Deleted

--- GeoWithResearch/GeoWithResearch/src/View.java
+++
@@ -1,8 +0,0 @@
-public class View {
-    public static void viewChildren(Person person, GeoTree geoTree){
-        System.out.println(person.getName() + " дети: " + new Research(geoTree).get(person, Relationship.child));
-    }
-    public static void viewParents(Person person, GeoTree geoTree){
-        System.out.println(person.getName() + " родители: " + new Research(geoTree).get(person, Relationship.parent));
-    }
-}
